9b057cdad685bf255619692b5e11dc910f16b650
Moharram <Moharum4@gmail.com>
2023-04-26 23:05:51 +0200

    hi

--- printf.c
+++ printf.c
@@ -19,9 +19,11 @@ int _printf(const char *format, ...)
 	unsigned int i;
 
 	va_start(ap,format);
+	counter = 0;
 
 	for (ch_char = format; *ch_char != '\0'; ch_char++)
 	{
+		/*
 		while (*ch_char != '%')
 		{
 			if (*ch_char != '\0')
@@ -39,36 +41,44 @@ int _printf(const char *format, ...)
 		}
 
 		ch_char++;
-		counter++;
-		
-		switch (*ch_char)
+		counter++;*/
+
+		if (*ch_char == '%')
 		{
-			case 'c':
-				i = va_arg(ap, int);
-				_putchar(i);
-				break;
+			ch_char++;
+			switch (*ch_char)
+			{
+				case 'c':
+					i = va_arg(ap, int);
+					counter += _putchar(i);
+					break;
 			
-			case 's':
-				s = va_arg(ap, char *);
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-				}
-				break;
+				case 's':
+					s = va_arg(ap, char *);
+					while (*s != '\0')
+					{
+						counter += _putchar(*s);
+						s++;
+					}
+					break;
 			
-			case 'i':
-				i = va_arg(ap, int);
-				putint(i);
-				break;
+				case 'i':
+					i = va_arg(ap, int);
+					putint(i);
+					break;
 			
-			case 'd':
-				i = va_arg(ap, int);
-				putint(i);
-				break;
+				case 'd':
+					i = va_arg(ap, int);
+					putint(i);
+					break;
+			}
+		}
+		else 
+		{
+			counter += _putchar(*ch_char);
 		}
 	}
 
 	va_end(ap);
-	return (counter - 3);
+	return (counter);
 }
